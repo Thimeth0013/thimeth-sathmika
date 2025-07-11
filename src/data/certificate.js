f2985b860d22c508ff63d17b3a3bc09eea03c899
Thimeth0013 <sathmikakb@gmail.com>
2025-07-12 01:30:23 +0530

    created a data file and added a certficate asset

---
+++ src/data/certificate.js
@@ -0,0 +1,14 @@
+import reactJsFundamentals from '../assets/certificates/reactJsFundamentals.jpg'
+
+const CertificateData = [
+  {
+    id: 1,
+    title: "React JavaScript - Fundamentals to Coding & New Beginnings",
+    description: "This free online web development course explains how to use Meta’s React JavaScript library to build user interfaces.",
+    thumbnail: reactJsFundamentals,
+    fullImage: reactJsFundamentals,
+  },
+
+];
+
+export default CertificateData;
