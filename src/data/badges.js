45f1f718fc36582b4b59ba475312064685d1f19c
Thimeth Sathmika <sathmikakb@gmail.com>
2025-08-24 09:20:39 +0530

    added Postman API Fundamentals badge

---
+++ src/data/badges.js
@@ -0,0 +1,16 @@
+import postmanApi from '../assets/badges/postmanApiFundamentals.png'
+
+const BadgesData = [
+  {
+    id: 1,
+    title: "Postman API Fundamentals Student Expert",
+    description: "Demonstrated proficiency in API testing, automation, and best practices using Postman. This badge validates skills in creating collections, writing tests, and managing API workflows effectively.",
+    image: postmanApi,
+    badgeUrl: "https://badgr.com/public/assertions/Z8I4La62RLuoQkH8vlPO_A",
+    issuer: "Postman",
+    issuerUrl: "https://www.postman.com/",
+    dateEarned: "August 2025",
+  }
+];
+
+export default BadgesData;
