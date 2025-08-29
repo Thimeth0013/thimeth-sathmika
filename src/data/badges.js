09705f4e2c0b7a7057c6fd8e41ff634ffc4de435
Thimeth Sathmika <sathmikakb@gmail.com>
2025-08-29 12:26:55 +0530

    added a new badge and updated display function

--- src/data/badges.js
+++ src/data/badges.js
@@ -1,16 +1,24 @@
-import postmanApi from '../assets/badges/postmanApiFundamentals.png'
-
 const BadgesData = [
   {
     id: 1,
     title: "Postman API Fundamentals Student Expert",
     description: "Demonstrated proficiency in API testing, automation, and best practices using Postman. This badge validates skills in creating collections, writing tests, and managing API workflows effectively.",
-    image: postmanApi,
+    image: "https://api.badgr.io/public/assertions/Z8I4La62RLuoQkH8vlPO_A/image",
     badgeUrl: "https://badgr.com/public/assertions/Z8I4La62RLuoQkH8vlPO_A",
     issuer: "Postman",
     issuerUrl: "https://www.postman.com/",
     dateEarned: "August 2025",
-  }
+  },
+  {
+    id: 2,
+    title: "Get Started with Postman Flows",
+    description: "This badge was awarded for completing the associated learning activities and hands-on challenge in the ‘Get Start with Postman Flows’ module in Postman Academy.",
+    image: "https://api.badgr.io/public/assertions/d5va3V0pQYeHoMVMY_2r4w/image",
+    badgeUrl: "https://api.badgr.io/public/assertions/d5va3V0pQYeHoMVMY_2r4w",
+    issuer: "Postman",
+    issuerUrl: "https://www.postman.com/",
+    dateEarned: "August 2025",
+  },
 ];
 
 export default BadgesData;
