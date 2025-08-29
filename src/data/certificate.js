e3acf8e2410ca6224473ac3eae3b4b85e8e1764a
Thimeth Sathmika <sathmikakb@gmail.com>
2025-08-29 12:27:31 +0530

    updated certificate links

--- src/data/certificate.js
+++ src/data/certificate.js
@@ -1,6 +1,4 @@
-import hpAgileProjectManagementPdf from '../assets/certificates/hpAgileProjectManagement.pdf'
 import hpAgileProjectManagementImg from '../assets/certificates/hpAgileProjectManagement.png'
-import hpAiForBeginnersPdf from '../assets/certificates/hpAiForBeginners.pdf'
 import hpAiForBeginnersImg from '../assets/certificates/hpAiForBeginners.png'
 
 const CertificateData = [
@@ -11,7 +9,7 @@ const CertificateData = [
     company: "HP LIFE",
     companyUrl: "https://www.life-global.org/",
     thumbnail: hpAgileProjectManagementImg,
-    pdf: hpAgileProjectManagementPdf,
+    certificateUrl: "https://www.life-global.org/certificate/f5d7c82c-2b8c-4a45-8227-34a0347b2ec1",
     date: "August 2025",
   },  
   {
@@ -21,8 +19,7 @@ const CertificateData = [
     company: "HP LIFE",
     companyUrl: "https://www.life-global.org/",
     thumbnail: hpAiForBeginnersImg,
-    pdf: hpAiForBeginnersPdf,
-    date: "August 2025",
+    certificateUrl: "https://www.life-global.org/certificate/09a1620f-bd9a-4f3c-9986-0f065e652e8b",
   }
 ];
 
