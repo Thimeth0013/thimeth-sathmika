eb16d758cafb123e5f55671609860a9aaa4db24b
Thimeth Sathmika <sathmikakb@gmail.com>
2025-11-23 08:43:39 +0530

    Updates

--- assets/CertificatesSection-DPViOtNV.js
+++ assets/CertificatesSection-DPViOtNV.js
@@ -1,4 +1,4 @@
-import{c as f,r as n,u as U,C as S,B as _,j as e,m as a,E as w}from"./index-BkR44dzd.js";/**
+import{c as f,r as n,u as U,C as S,B as _,j as e,m as a,E as w}from"./index-CQjV6_SZ.js";/**
  * @license lucide-react v0.522.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/SkillsSection-BNxDCLNV.js
+++ assets/SkillsSection-BNxDCLNV.js
@@ -1,4 +1,4 @@
-import{c as q,r as o,j as a,u as J,m}from"./index-BkR44dzd.js";/**
+import{c as q,r as o,j as a,u as J,m}from"./index-CQjV6_SZ.js";/**
  * @license lucide-react v0.522.0 - ISC
  *
  * This source code is licensed under the ISC license.
