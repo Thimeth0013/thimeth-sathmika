a2b21a12b64193a149d42de9d86910da7bd1d515
Thimeth Sathmika <sathmikakb@gmail.com>
2025-12-10 08:02:51 +0530

    Updates

--- assets/CertificatesSection-CtaQmQ90.js
+++ assets/CertificatesSection-CtaQmQ90.js
@@ -1,4 +1,4 @@
-import{c as f,r as n,u as U,C as S,B as _,j as e,m as a,E as w}from"./index-CcMzjxPS.js";/**
+import{c as f,r as n,u as U,C as S,B as _,j as e,m as a,E as w}from"./index-ClIDe2sM.js";/**
  * @license lucide-react v0.522.0 - ISC
  *
  * This source code is licensed under the ISC license.
