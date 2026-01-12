8e390ef66e86cfa25150e5187d51d33db2113dc6
Thimeth Sathmika <sathmikakb@gmail.com>
2026-01-12 20:52:33 +0530

    Updates

--- assets/CertificatesSection-DojTtIQ0.js
+++ assets/CertificatesSection-DojTtIQ0.js
@@ -1,4 +1,4 @@
-import{c as p,r as h,C as w,B as v,j as e,m as g,A as N,a as C,E as _}from"./index-BZQ7w6bE.js";/**
+import{c as p,r as h,C as w,B as v,j as e,m as g,A as N,a as C,E as _}from"./index-BNKB6OeR.js";/**
  * @license lucide-react v0.522.0 - ISC
  *
  * This source code is licensed under the ISC license.
