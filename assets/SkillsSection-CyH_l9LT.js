473e39dc39038c61d6c671eac1496312020840de
Thimeth Sathmika <sathmikakb@gmail.com>
2026-03-05 20:45:13 +0530

    Updates

--- assets/SkillsSection-CyH_l9LT.js
+++ assets/SkillsSection-CyH_l9LT.js
@@ -1,4 +1,4 @@
-import{c as N,r as s,j as i,u as U,m as w}from"./index-DcEWeQO4.js";/**
+import{c as N,r as s,j as i,u as U,m as w}from"./index-CR975iZv.js";/**
  * @license lucide-react v0.522.0 - ISC
  *
  * This source code is licensed under the ISC license.
